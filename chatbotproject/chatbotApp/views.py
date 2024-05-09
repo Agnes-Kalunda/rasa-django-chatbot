a641e1f9634621796e5ce03496623c054fc8ead1
Agnes <agypeter97@gmail.com>
2024-05-09 19:50:02 +0300

    containerize django app

--- chatbotproject/chatbotApp/views.py
+++ chatbotproject/chatbotApp/views.py
@@ -1,5 +1,9 @@
+# views.py
 from django.shortcuts import render
 
-# Create your views here.
-def index ( request):
-    return render (request, "index.html")
+
+
+
+
+def index(request):
+    return render(request, "index.html")
--- chatbotproject/chatbotproject/settings.py
+++ chatbotproject/chatbotproject/settings.py
@@ -11,10 +11,13 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+from dotenv import load_dotenv
+import os
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+load_dotenv()
 
 
 # Quick-start development settings - unsuitable for production
@@ -88,8 +91,11 @@ WSGI_APPLICATION = 'chatbotproject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': os.getenv('DB_HOST'),
     }
 }
 
